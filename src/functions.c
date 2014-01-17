c35e7b97c9c06255bbaecec58069f604785c08ef
Feng Wen <fengwen@usc.edu>
2014-01-16 21:42:30 -0800

    hexdumpFile implemented

--- src/functions.c
+++ src/functions.c
@@ -9,8 +9,66 @@ void decBase64Stdin(){
 	printf("dec-base64Stdin()\n");
 }
 void hexdumpFile(char *fileName){
-	printf("hexdumpStdin()\n");
-	printf("File Name: %s\n", fileName);
+	FILE *ifp = NULL;
+	char *mode = "r";
+	unsigned char ch = 0;
+	ifp = fopen(fileName, mode);
+	int count = 0;
+	char buff[17];
+	
+	if(ifp == NULL){
+		fprintf(stderr, "Can't open %s\n", fileName);
+		return;	
+	}
+
+
+	while(!feof(ifp)){
+		
+		if(count % 16 == 0){
+			
+			if(count != 0){
+				buff[16] = 0;
+				printf("%s\n", buff);			
+			}	
+			printf("%06x: ", count);
+		}
+		ch = fgetc(ifp);
+		if(feof(ifp)){
+			break;		
+		}
+		printf("%02x ", ch);
+		if(ch <= 0x1f || ch == 0x7f){
+			ch = '.';
+		}
+		else{ 
+			if(ch >= 0x80){
+				ch = '~';
+			}
+			
+		}
+		
+		buff[count % 16] = ch;
+		count++;
+		if(count % 8 == 0){
+			putchar(' ');		
+		}
+		
+	}
+	while(count % 16 != 0){		
+		if(count % 8 == 0){
+			putchar(' ');
+		}		
+		buff[count % 16] = ' ';
+		printf("-- ");
+				
+		count++;
+		
+	}
+	buff[16] = 0;
+	printf(" %s\n", buff);
+	
+	fclose(ifp);
+
 }
 void encBase64File(char *fileName){
 	printf("encBase64File()\n");
