26c975ae3f79b775bdef4658772c9463a70fe1a3
Feng Wen <fengwen@usc.edu>
2014-01-16 19:26:10 -0800

    add src include/

---
+++ src/functions.c
@@ -0,0 +1,22 @@
+#include "hw1.h"
+void hexdumpStdin(){
+	printf("hexdumpStdin()\n");
+}
+void encBase64Stdin(){
+	printf("enc-base64Stdin()\n");
+}
+void decBase64Stdin(){
+	printf("dec-base64Stdin()\n");
+}
+void hexdumpFile(char *fileName){
+	printf("hexdumpStdin()\n");
+	printf("File Name: %s\n", fileName);
+}
+void encBase64File(char *fileName){
+	printf("encBase64File()\n");
+	printf("File Name: %s\n", fileName);
+}
+void decBase64File(char *fileName){
+	printf("decBase64File()\n");
+	printf("File Name: %s\n", fileName);
+}
---
+++ src/hw1.c
@@ -0,0 +1,62 @@
+#include "hw1.h"
+
+int main(int argc, char *argv[]){
+	switch(argc){
+	case 1: printf("Error: Lack of argument(s) (can be hexdump | enc-base64 | dec-base64)\n"); break;
+	case 2: oneArgument(argv); break;
+	case 3: twoArguments(argv); break;
+	default: printf("Error: Too many arguments (the number of arguments should be less than or equal to 2)\n");
+	}		
+	return 0;
+}
+
+void oneArgument(char *argv[]){
+	if(strcmp(argv[1], "hexdump") == 0){
+		hexdumpStdin();	
+	}
+	else{
+		if(strcmp(argv[1], "enc-base64") == 0){
+			encBase64Stdin();	
+		}
+		else{
+			if(strcmp(argv[1], "dec-base64") == 0){
+				decBase64Stdin();
+			}
+			else{
+				printf("Error: the first argument should be hexdump | enc-base64 | dec-base64)\n");
+			}		
+		}
+	}
+}
+
+void twoArguments(char *argv[]){
+	int funcFlag = 0;
+	if(strcmp(argv[1], "hexdump") == 0){
+		funcFlag = 1;
+	}
+	else{
+		if(strcmp(argv[1], "enc-base64") == 0){
+			funcFlag = 2;	
+		}
+		else{
+			if(strcmp(argv[1], "dec-base64") == 0){
+				funcFlag = 3;
+			}
+			else{
+				printf("Error: the first argument should be hexdump | enc-base64 | dec-base64)\n");
+				return;
+			}		
+		}
+	}
+	
+	if(access(argv[2], F_OK) != -1){
+		switch(funcFlag){
+		case 1: hexdumpFile(argv[2]);break;
+		case 2: encBase64File(argv[2]);break;
+		case 3: decBase64File(argv[2]);break;
+		}
+	}
+	else{
+		printf("Error: this file doesn't exsit\n");	
+	}
+}
